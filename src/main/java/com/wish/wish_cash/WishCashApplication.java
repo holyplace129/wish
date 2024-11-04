91c6c7eb35b0462e27cd529244b917dc46c78630
holyplace <kwonseongji129@gmail.com>
2024-11-04 19:50:49 +0900

    first commit

---
+++ src/main/java/com/wish/wish_cash/WishCashApplication.java
@@ -0,0 +1,13 @@
+package com.wish.wish_cash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishCashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishCashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wish/wish_cash/WishCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wish.wish_cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishCashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
